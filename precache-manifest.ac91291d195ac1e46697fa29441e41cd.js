5edecaa3e35895e055b6e4039269b0754fe25579
Vadym Kalmykov <drave.beneth@gmail.com>
2020-08-30 14:06:20 +0300

    Updates

---
+++ precache-manifest.ac91291d195ac1e46697fa29441e41cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e0a9f8b103d6edb16701bc9b1c81932",
+    "url": "/helper2/index.html"
+  },
+  {
+    "revision": "93ea193a44d0c32872f1",
+    "url": "/helper2/static/css/main.5d7d7745.chunk.css"
+  },
+  {
+    "revision": "1a3fa92bcb9c754438d5",
+    "url": "/helper2/static/js/2.c049c454.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/helper2/static/js/2.c049c454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ea193a44d0c32872f1",
+    "url": "/helper2/static/js/main.c739c2cf.chunk.js"
+  },
+  {
+    "revision": "7765e7516a20974b3db4",
+    "url": "/helper2/static/js/runtime-main.08d16b8d.js"
+  },
+  {
+    "revision": "8648c684069f739969a04ab7088018f9",
+    "url": "/helper2/static/media/copy.8648c684.svg"
+  },
+  {
+    "revision": "aed532819e771b71dc9a8de88aae793b",
+    "url": "/helper2/static/media/print.aed53281.svg"
+  }
+]);
